c4770efd7343ca1c7edf0692806bb8173aae45b1
aasibmansuri <160222398+aasibmansuri@users.noreply.github.com>
2025-04-12 14:37:30 +0530

    Create practical2.java

---
+++ practical2.java
@@ -0,0 +1,15 @@
+class practical2{
+    public static void main(String []args){
+        int a=10;
+        int b=20;
+        int c=30;
+        if (a>b && a>c){
+            System.out.println("a is max"+a);
+        }else if (b>a && b>c){
+            System.out.println("b is max "+b);
+        }else {
+            System.out.println("c is max"+c);
+        }
+
+    }
+}
