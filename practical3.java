e4b3b0df6e73cacdd9102f0445e76095563f4227
aasibmansuri <160222398+aasibmansuri@users.noreply.github.com>
2025-04-12 14:38:07 +0530

    Create practical3.java

---
+++ practical3.java
@@ -0,0 +1,11 @@
+class practical3 {
+
+    public static void main(String[] args) {
+        int c = 23456;
+        while (c > 0) {
+            int a = c % 10;
+            c = c * a;
+
+        }
+    }
+}
