1bdcc508328cd21faa35a6681edae70dd73b51e5
aasibmansuri <160222398+aasibmansuri@users.noreply.github.com>
2025-05-18 20:54:47 +0530

    Add files via upload

---
+++ practical7.java
@@ -0,0 +1,12 @@
+public class practical7 {
+  int x;
+
+  public practical7(int x) {
+    this.x = x;
+  }
+
+  public static void main(String[] args) {
+    practical7 myObj = new practical7(5);
+    System.out.println("Value of x = " + myObj.x);
+  }
+}
